2aa34f73c8ae77bb55da7d08825e5f37c9062b97
fnannizz <francesca.nannizzi@mathworks.com>
2015-10-02 15:14:51 -0400

    Adding gradle to mw path.

--- mw/mw.sh
+++ mw/mw.sh
@@ -8,3 +8,12 @@ alias setand="setmwe -a android-arm-32 pwd"
 #   Application-specific 
 #   -----------------------------
 alias xc="open -a /Applications/Xcode6.2.0.app/ "
+
+#   -----------------------------
+#   Android Development
+#   -----------------------------
+
+# Added for Gradle
+export PATH="/sandbox/fnannizz/android/gradle-2.5/bin:$PATH"
+
+cd /sandbox/fnannizz
