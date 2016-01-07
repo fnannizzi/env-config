11273a8fb9416758767ace2177e6c553893799b4
fnannizz <francesca.nannizzi@mathworks.com>
2016-01-07 14:32:28 -0500

    Use later version of emacs at work.

--- mw/mw.sh
+++ mw/mw.sh
@@ -4,12 +4,22 @@
 alias setmac="setmwe -a maci64 pwd"
 alias setand="setmwe -a android-arm-32 pwd"
 alias setios="setmwe -a ios-arm-64 pwd"
-export P4MERGE="/Applications/p4merge.app/Contents/MacOS/p4merge"
+
+#   -----------------------------
+#   Perforce 
+#   -----------------------------
+#export P4MERGE="/Applications/p4merge.app/Contents/MacOS/p4merge"
+export P4MERGE="twdiff --wait"
+export EDITOR="edit --wait"
+export P4EDITOR="edit --wait"
+export P4DIFF="twdiff --wait"
 
 #   -----------------------------
 #   Application-specific 
 #   -----------------------------
 alias xc="open -a /Applications/Xcode6.2.0.app/ "
+alias em='/Applications/Emacs_24.3.app/Contents/MacOS/Emacs -nw'
+alias emacs='/Applications/Emacs_24.3.app/Contents/MacOS/Emacs -nw'
 
 #   -----------------------------
 #   Android Development
