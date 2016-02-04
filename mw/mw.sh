c5b94ebdfa3afba7f6b6ff220ca27a665209ed76
fnannizz <francesca.nannizzi@mathworks.com>
2016-02-04 14:20:28 -0500

    Add paths to frequently used tools.

--- mw/mw.sh
+++ mw/mw.sh
@@ -24,6 +24,13 @@ alias emacs='/Applications/Emacs_24.3.app/Contents/MacOS/Emacs -nw'
 #   -----------------------------
 #   Android Development
 #   -----------------------------
+alias ndk_root="/sandbox/fnannizz/android/android-ndk-r10e"
+alias ndk_tool_root="$NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin"
+alias sdk_root="/sandbox/fnannizz/android/android-sdk"
+
+alias aadb="$SDK_ROOT/platform-tools/adb"
+alias anm="$NDK_TOOL_ROOT/arm-linux-androideabi-gcc-nm"
+alias areadelf="$NDK_TOOL_ROOT/arm-linux-androideabi-readelf"
 
 # Added for Gradle
 export PATH="/sandbox/fnannizz/android/gradle-2.5/bin:$PATH"
