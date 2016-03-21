386ec768e933b843f795bdc5ad62454b437ef2c3
fnannizz <francesca.nannizzi@mathworks.com>
2016-03-21 09:17:26 -0400

    Add new Android tool aliases.

--- mw/mw.sh
+++ mw/mw.sh
@@ -8,29 +8,29 @@ alias setios="setmwe -a ios-arm-64 pwd"
 #   -----------------------------
 #   Perforce 
 #   -----------------------------
-#export P4MERGE="/Applications/p4merge.app/Contents/MacOS/p4merge"
-export P4MERGE="twdiff --wait"
-export EDITOR="edit --wait"
-export P4EDITOR="edit --wait"
-export P4DIFF="twdiff --wait"
+export P4MERGE="/Applications/p4merge.app/Contents/MacOS/p4merge"
+export EDITOR="emacs -nw"
+export P4EDITOR="emacs -nw"
 
 #   -----------------------------
 #   Application-specific 
 #   -----------------------------
 alias xc="open -a /Applications/Xcode6.2.0.app/ "
 alias em='/Applications/Emacs_24.3.app/Contents/MacOS/Emacs -nw'
-alias emacs='/Applications/Emacs_24.3.app/Contents/MacOS/Emacs -nw'
+alias emacs='/Applications/Emacs_24.3.app/Contents/MacOS/Emacs'
 
 #   -----------------------------
 #   Android Development
 #   -----------------------------
-alias ndk_root="/sandbox/fnannizz/android/android-ndk-r10e"
-alias ndk_tool_root="$NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin"
-alias sdk_root="/sandbox/fnannizz/android/android-sdk"
+export NDK_ROOT="/sandbox/fnannizz/android/android-sdk/ndk-bundle"
+export NDK_TOOL_ROOT="$NDK_ROOT/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64/bin"
+export SDK_ROOT="/sandbox/fnannizz/android/android-sdk"
 
 alias aadb="$SDK_ROOT/platform-tools/adb"
 alias anm="$NDK_TOOL_ROOT/arm-linux-androideabi-gcc-nm"
 alias areadelf="$NDK_TOOL_ROOT/arm-linux-androideabi-readelf"
+alias aaddr2line="$NDK_TOOL_ROOT/arm-linux-androideabi-addr2line"
+alias acppfilt="$NDK_TOOL_ROOT/arm-linux-androideabi-c++filt"
 
 # Added for Gradle
 export PATH="/sandbox/fnannizz/android/gradle-2.5/bin:$PATH"
