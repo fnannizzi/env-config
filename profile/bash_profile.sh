49eeb7e0d96fd5990b9782c12282f7bef3562939
fnannizz <francesca.nannizzi@mathworks.com>
2016-05-25 14:29:41 -0400

    Add errred to make stderr output red

--- profile/bash_profile.sh
+++ profile/bash_profile.sh
@@ -63,6 +63,12 @@ trash () {
     mv "$1" ~/.Trash
 }
 
+#   errred: Make stderr output red
+#   ------------------------------------------------------
+errred () {
+    (set -o pipefail;"$@" 2>&1>&3|sed $'s,.*,\e[31m&\e[m,'>&2)3>&1
+}
+
 case "$OSTYPE" in
     darwin*)
         #   beep: Make the terminal bell sound
